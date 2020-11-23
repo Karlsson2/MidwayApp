62ac214afc12ea3d159cc93aa8965ee7cb3f721b
Fanny Karlsson <fanny.ll.karlsson@gmail.com>
2020-11-23 16:57:04 +0000

    setup devise

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -109,4 +109,7 @@ Rails.application.configure do
   # config.active_record.database_selector = { delay: 2.seconds }
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
+
+  # config for devise
+  config.action_mailer.default_url_options = { host: 'https://www.mid-way.co.uk/' }
 end
