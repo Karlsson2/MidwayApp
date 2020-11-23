68f58a2b98b7ee0d0a6824a9bf8438cff776fe8a
Matt Daloisio <mattdam1@yahoo.co.uk>
2020-11-23 17:57:37 +0000

    user changes

--- db/migrate/20201123171601_devise_create_users.rb
+++ db/migrate/20201123171601_devise_create_users.rb
@@ -6,6 +6,10 @@ class DeviseCreateUsers < ActiveRecord::Migration[6.0]
       ## Database authenticatable
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
+      t.string :first_name
+      t.string :last_name
+      t.string :username
+      t.string :location
 
       ## Recoverable
       t.string   :reset_password_token
