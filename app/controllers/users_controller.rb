d14253d2a83a12fbd7bb35c1f842315ba1f0d550
Saskia Knauf <saskia.knauf@gmail.com>
2020-11-24 17:47:48 +0000

    adding and seeing friends page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,4 +3,9 @@ class UsersController < ApplicationController
   def show
     @user = User.find(current_user.id)
   end
+
+  def friends
+    @friends = current_user.friends
+    @not_friends = current_user.not_friends_of_user
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,13 @@
 Rails.application.routes.draw do
   devise_for :users
 
-  get 'users/:id' => 'users#show', as: "user_profile"
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-
   root to: 'pages#home'
+
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :users do
+    resources :friendships, only: [ :create ]
+  end
+
+  get 'users/:id' => 'users#show', as: "user_profile"
+  get 'friends', to: 'users#friends', as: 'friends'
 end
