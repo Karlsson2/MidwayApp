0a67b18dbd93b23519ee4b34a6a4d04b6afd6449
Har Dhillon <har7173@gmail.com>
2020-11-24 11:48:00 +0000

    devise views

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,18 @@
 class ApplicationController < ActionController::Base
+  
+  # >>> Uncomment for whitelist approach 
+  # before_action :authenticate_user!
+
+  # Adding first name and last name upon registration
+  
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  def configure_permitted_parameters
+    # For additional fields in app/views/devise/registrations/new.html.erb
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name])
+
+    # For additional in app/views/devise/registrations/edit.html.erb
+    devise_parameter_sanitizer.permit(:account_update, keys: [:username])
+  end
+
 end
