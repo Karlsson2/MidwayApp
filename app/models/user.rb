faf7c6bba184a1f097de47c5dd0909f4d62fb8e6
Har Dhillon <har7173@gmail.com>
2020-11-25 16:31:57 +0000

    made divs on profile page

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   has_one_attached :photo
 
-  validates :username, , uniqueness: true
+  validates :username, uniqueness: :true
 
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
