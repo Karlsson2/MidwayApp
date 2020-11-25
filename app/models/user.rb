010caa3c99a64dcb96068731b27bb1b40aa212ed
Har Dhillon <har7173@gmail.com>
2020-11-25 18:35:32 +0000

    styled profile page

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   has_one_attached :photo
 
+  validates :username, uniqueness: :true
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
