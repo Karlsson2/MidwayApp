813488f1dc179949ebe3cdcf4998450d75d41070
Matt Daloisio <mattdam1@yahoo.co.uk>
2020-11-25 17:06:05 +0000

    working midpoint calculator

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,10 @@ Rails.application.routes.draw do
     resources :friendships, only: [ :create ]
   end
 
+  resources :midways, only: [:index, :new, :create]
+
   get 'users/:id' => 'users#show', as: "user_profile"
   get 'friends', to: 'users#friends', as: 'friends'
+  get 'choose_venue', to: 'midways#choose_venue', as: 'choose_venue'
+
 end
---
+++ test/controllers/midways_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class MidwayControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
