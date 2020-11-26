23e5c8196eb70653af52c7702882ec7ecccc8d0e
Matt Daloisio <mattdam1@yahoo.co.uk>
2020-11-26 18:09:00 +0000

    final category

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_26_145923) do
+ActiveRecord::Schema.define(version: 2020_11_26_174214) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -63,6 +63,7 @@ ActiveRecord::Schema.define(version: 2020_11_26_145923) do
     t.datetime "updated_at", precision: 6, null: false
     t.string "time_option"
     t.datetime "future_time"
+    t.string "venue_type"
     t.index ["user_id"], name: "index_midways_on_user_id"
   end
 
