6c5db962d8c9834118d2d4256c485331fddc8359
Fanny Karlsson <fanny.ll.karlsson@gmail.com>
2020-12-02 14:24:59 +0000

    fixed dashboards so it displays custom messages for people with no midways

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,24 +1,21 @@
 class PagesController < ApplicationController
-
   def home
     @navbar_visible = false
   end
 
   def dashboard
-
-    participants = current_user.midways.last.midway_participants
-    @participant_string = ""
-    participants.each_with_index do |participant, index|
-
-    if index == participants.length - 1
-        @participant_string << "& #{participant.user.first_name}"
-      elsif index == participants.length - 2
-        @participant_string << "#{participant.user.first_name} "
-      else
-        @participant_string << "#{participant.user.first_name}, "
+    if current_user.midways.count > 0
+      participants = current_user.midways.last.midway_participants
+      @participant_string = ""
+      participants.each_with_index do |participant, index|
+        if index == participants.length - 1
+          @participant_string << "& #{participant.user.first_name}"
+        elsif index == participants.length - 2
+          @participant_string << "#{participant.user.first_name} "
+        else
+          @participant_string << "#{participant.user.first_name}, "
+        end
       end
+    end
   end
-
-
-end
 end
